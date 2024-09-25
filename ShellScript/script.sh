0bec0fe57fd74421fe8758e8ac2d2357035af229
vittoriolaferla <vittorio.laferla@mail.polimi.it>
2024-09-25 11:41:43 +0200

    Adding Script job

---
+++ ShellScript/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -J firstExample
+#SBATCH -e /cluster/home/vlaferla/fdsEulerJobs/0/firstExample.err
+#SBATCH -o /cluster/home/vlaferla/fdsEulerJobs/0/firstExample.log
+#SBATCH --partition=<name of queue>
+#SBATCH --ntasks=4
+#SBATCH --nodes=2
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --time=2:0:0
+export OMP_NUM_THREADS=1
+cd <pwd>
+srun -N 2 -n 4 --ntasks-per-node 2 /cluster/home/vlaferla/fdsEulerJobs/0 \0\0.fds 
